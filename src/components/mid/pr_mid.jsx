9eed96a47b6f05efa6578cc40fccd8182688d53c
Harkaran <harkaran0010@gmail.com>
2023-04-20 14:28:33 +0530

    Temp

--- src/components/mid/pr_mid.jsx
+++ src/components/mid/pr_mid.jsx
@@ -60,6 +60,12 @@ export default function PRMID() {
 
           <MAINF/>
           <MAINF/>
+          <MAINF/>
+          <MAINF/>
+          <MAINF/>
+          <MAINF/>
+          <MAINF/>
+          <MAINF/>
           
           
         </div>
